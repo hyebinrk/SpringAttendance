29b6415268c1b3290daf32d69d1e49219e32bdf7
hyebinrk <gpqls3698@naver.com>
2025-05-29 01:51:20 +0900

    2일차

--- DBeaver/테이블, 테스트용 유저 생성.sql
+++ DBeaver/테이블, 테스트용 유저 생성.sql
@@ -1,9 +1,14 @@
+/* 시퀀스 생성 */
+CREATE SEQUENCE users_seq
+	START WITH 1		/* 1부터시작 */
+	INCREMENT BY 1		/* 1씩 증가 */
+	NOCACHE				/* 캐시하지않음(안정성 우선) */
+	NOCYCLE;			/* 반복하지않음 */
+
+/* 테이블 생성 */
 CREATE TABLE users (
-	id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	username VARCHAR2(50) NOT NULL UNIQUE,
-	password VARCHAR2(100) NOT NULL
+	id NUMBER PRIMARY KEY,
+	username varchar2(50) NOT NULL UNIQUE,
+	password varchar2(100) NOT null
 );
 
--- 테스트용 유저
-INSERT INTO users (username, password) VALUES ('admin', '1234');
-COMMIT;
